072f6e97b1ade1be58d6e3f2232616279e82de3c
c.georgak727 <c.georgak727@gmail.com>
2026-06-24 17:00:41 +0300

    FIX/ backend error

--- backend/src/helpers/constants.ts
+++ backend/src/helpers/constants.ts
@@ -3,4 +3,6 @@ export const BASE_URL = '/api'
 export const allowedOrigins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:80",
+    "http://127.0.0.1:80",
 ];
